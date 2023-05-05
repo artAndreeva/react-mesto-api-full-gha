425de7d80d8df3a9bdef2eb622c25291cdc23f50
Svetlana Andreeva <5231328@gmail.com>
2023-05-05 17:40:49 +0300

    hotfix: add crush

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use(authRouter);
 app.use(auth);
 app.use(router);
