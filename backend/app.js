2491d57b0877c7bfd6a06a419535c7bbfa7269cb
Svetlana Andreeva <5231328@gmail.com>
2023-05-09 15:47:59 +0300

    fix

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,8 @@ const setHeaders = require('./middlewares/cors');
 
 const app = express();
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
+// const { PORT = 3001 } = process.env;
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
